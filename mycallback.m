c6b56827320a8eb534f4cfdd56eabdd28ae7a643
fuoar <amaraouf@gmail.com>
2022-05-27 06:20:29 +0200

    planes noOK; Boltzmann OK

---
+++ mycallback.m
@@ -0,0 +1,25 @@
+function [res] = myfunction(obj,event_obj)
+% obj          handle to the object that has been clicked on
+% event_obj    handle to event data object (empty in this release)
+% res [output] logical flag to determine whether the rotate
+%              operation should take place or the 'ButtonDownFcn'
+%              property of the object should take precedence
+%turn pan off
+
+						ggg= event_obj.Axes.View;
+			%sprintf('Current view az %d el %d',ggg)
+			azel=ggg;
+			assignin('base','gg',event_obj.Axes.View)
+			%azel = reshape(ggg,[2 1]);
+			azel(1) = mod(azel(1),360)-180;
+			ttmp=azel2phitheta(reshape(azel,[2,1]),false);
+
+			%sprintf('Current view phi %d theta %d',ttmp)
+			%ttmp=azel2phitheta([azel(1); azel(2)],false);
+
+			app.PhiEditField.Value = mod(ttmp(1)-270,360);
+			app.ThetaEditField.Value = ttmp(2);
+
+
+			UpdateLinePlot(app)
+end
--- test.m
+++ test.m
@@ -4,7 +4,6 @@ hLine = plot(rand(1,10),'ButtonDownFcn','disp(''This executes'')');
 hLine.Tag = 'DoNotIgnore';
 h = rotate3d;
 h.ButtonDownFilter = @mycallback;
-h.ActionPostCallback =@mycallback;
 h.Enable = 'on';
 % mouse-click on the line
 function [flag] = mycallback(obj,event_obj)
@@ -14,4 +13,6 @@ if strcmpi(objTag,'DoNotIgnore')
    flag = true;
 else
    flag = false;
+end
+end
 end
