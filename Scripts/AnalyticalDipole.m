11c14c017bf1ad0d3363a8991b8a915ece3ab1ba
fuoar <amaraouf@gmail.com>
2022-06-06 22:08:58 +0200

    polarisation for all observation angles calculated

--- Scripts/AnalyticalDipole.m
+++ Scripts/AnalyticalDipole.m
@@ -10,12 +10,13 @@ clrmp = @(x) brewermap(x,"PuOr");
 %% Parameters
 
 % dipole orientation
-theta_dip = pi/8;
+theta_dip = pi/2;
 phi_dip = pi/2;
 
-% n1 = 1.5;	% PS or SiO2
-% n2 = 1;		% air
+n1 = 1.5;	% PS or SiO2
+n2 = 1;		% air
 
+f = 2e-3; % in m
 %% Create variables
 
 u_dip = [sin(theta_dip)*cos(phi_dip); ...
@@ -42,7 +43,7 @@ Emat_s = zeros(length(thetas_obs),length(phis_obs));	% norme du champ selon s
 Emat_p = zeros(length(thetas_obs),length(phis_obs));	% norme du champ selon p
 Emat_3tot = zeros(length(thetas_obs),length(phis_obs),3);
 
-
+Pol = zeros(length(thetas_obs),length(phis_obs),1e2);
 
 %% loop over all observation directions
 for i=1:length(thetas_obs)
@@ -50,6 +51,8 @@ for i=1:length(thetas_obs)
 		theta_obs = thetas_obs(i);
 		phi_obs = phis_obs(j);
 
+
+theta_obs2 = asin(n2./n1.*sin(theta_obs));
 		% unit vector in observation direction
 		u_obs = [sin(theta_obs)*cos(phi_obs); ...
 			sin(theta_obs)*sin(phi_obs); ...
@@ -88,14 +91,48 @@ for i=1:length(thetas_obs)
 		Emat_s(i,j) = norm(E_s_mat*u_s);
 		Emat_p(i,j) = norm(E_p_mat*u_p);
 
-		%%%% from definition and projection
-		
+%%%%%
+% after objective
+v_s = u_s;
 
+v_p = [cos(phi_obs);...
+	sin(phi_obs);...
+	0];
+
+rho = f*sin(theta_obs);
+
+Eobj = sqrt(n2./cos(theta_obs2)).*(dot(E_tot_fs,v_s)*v_s+dot(E_tot_fs,v_p)*v_p);
+%size(Eobj)
+	
+		%%% projection on polariser axis
+
+	%	figure(4)
+		%if j<i
+		%Pol = [];
+		alpha = linspace(0,2*pi,1e2);
+		for kk=1:length(alpha)%alpha=linspace(0,2*pi,1e2)
+		u_alpha = [ cos(alpha(kk));...
+			sin(alpha(kk));...
+			0];
+
+		%%% from def
+		Pol(i,j,kk) = dot(Eobj,u_alpha).^2;
+		end
+
+
+%polarplot(linspace(0,2*pi,1e2),Pol)
+%hold on
+		%end
+
+	
+	
 	end
 	DOP(i) = getDOP(Emat_tot(i,:));
 end
 
 
+
+% figure
 %% Store simulation results
 
 Simu.Date = sprintf('%s',datetime);
@@ -113,22 +150,25 @@ Simu.Emat_s = Emat_s;
 Simu.Emat_p = Emat_p;
 
 
+Simu.Pol = Pol;
+
+
 clearvars -except Simu clrmp
 
 %% Show calculated results
 
 
-PlotResults(Simu,'Lethiec',clrmp)
+[x,y,z]=PlotResults(Simu,'Lethiec',clrmp);
 
-PlotResults(Simu,'fromdef',clrmp)
-
-
+Simu.dip.x = x; 
+Simu.dip.y = y;
+Simu.dip.z = z;
 
 %% Function
 clearvars -except Simu
 
 
-function PlotResults(Simu,mode,clrmp)
+function [x,y,z] = PlotResults(Simu,mode,clrmp)
 
 switch mode
 	case 'Lethiec'
@@ -156,9 +196,9 @@ Edef_s = Simu.Edef_s;
 Edef_p = Simu.Edef_p;
 
 end
-
-figure
-patternCustom(Edef_tot',rad2deg(thetas_obs), rad2deg(phis_obs))
+% 
+% figure
+% patternCustom(Edef_tot',rad2deg(thetas_obs), rad2deg(phis_obs))
 
 % figure
 % nexttile
@@ -171,30 +211,38 @@ patternCustom(Edef_tot',rad2deg(thetas_obs), rad2deg(phis_obs))
 % imagesc(rad2deg(Simu.thetaphi_obs(1,:)),rad2deg(Simu.thetaphi_obs(2,:)),Simu.Edef_p')
 % colorbar; caxis([0 1])
 
-figure('Position',[1377 114.6000 687.2000 1.0176e+03], 'Color','w');
+figure('Position',[712.2000 49 1.3520e+03 1.0832e+03], 'Color','w');
 tiledlayout('flow','TileSpacing','compact','Padding','compact');
 
-
-nexttile
-p=pcolor(rad2deg(thetas_obs)-rad2deg(theta_dip),rad2deg(phis_obs)-rad2deg(phi_dip),Edef_tot');
-p.EdgeColor = 'none';
-colorbar; caxis([0 1]);
-xlabel('\Theta_{obs} - \Theta_{dip}','Interpreter','tex');
-ylabel('\Phi_{obs} - \Phi_{dip}','Interpreter','tex');
-
-title('Norm of E vs \Theta_{obs} and \Phi_{obs}','interpreter','tex')
-subtitle(sprintf('\\Theta_{dip} = %d and \\Phi_{dip} = %d', rad2deg(theta_dip), rad2deg(phi_dip)));%,'HorizontalAlignment','left');
-
-ax=gca; ax.TitleHorizontalAlignment = 'left'; 
-%colormap(brewermap([],"PuOr"))
-
-
-nexttile
-ss=surfc(rad2deg(thetas_obs), rad2deg(phis_obs), Edef_tot');
-ss(1).EdgeColor='none';
-xlabel('\Theta_{obs}','Interpreter','tex');
-ylabel('\Phi_{obs}','Interpreter','tex');
-zlabel('|E|')
+% 
+% azel = phitheta2azel([rad2deg(phis_obs);rad2deg(thetas_obs)],false);
+% 
+nexttile([2 2])
+[x,y,z] = spherical2cart(Edef_tot',thetas_obs,phis_obs);
+s=surf(x,y,z,Edef_tot);
+s(1).EdgeColor='none'; colorbar; colormap(clrmp([]))
+pbaspect([1 1 1])
+% 
+% nexttile
+% p=pcolor(rad2deg(thetas_obs)-rad2deg(theta_dip),rad2deg(phis_obs)-rad2deg(phi_dip),Edef_tot');
+% p.EdgeColor = 'none';
+% colorbar; caxis([0 1]);
+% xlabel('\Theta_{obs} - \Theta_{dip}','Interpreter','tex');
+% ylabel('\Phi_{obs} - \Phi_{dip}','Interpreter','tex');
+% 
+% title('Norm of E vs \Theta_{obs} and \Phi_{obs}','interpreter','tex')
+% subtitle(sprintf('\\Theta_{dip} = %d and \\Phi_{dip} = %d', rad2deg(theta_dip), rad2deg(phi_dip)));%,'HorizontalAlignment','left');
+% 
+% ax=gca; ax.TitleHorizontalAlignment = 'left'; 
+% %colormap(brewermap([],"PuOr"))
+% 
+% 
+% nexttile
+% ss=surfc(rad2deg(thetas_obs), rad2deg(phis_obs), Edef_tot');
+% ss(1).EdgeColor='none';
+% xlabel('\Theta_{obs}','Interpreter','tex');
+% ylabel('\Phi_{obs}','Interpreter','tex');
+% zlabel('|E|')
 
 
 
@@ -219,7 +267,6 @@ ll = length(ax.Children); cg = clrmp(ll);%cgrad3([140 81 10]./255, [0.8 0.8 0.8]
 ax.ColorOrder = cg;
 
 
-
 nexttile
 for i=1:5:size(Edef_s,2)
 polarplot(thetas_obs,Edef_s(:,i));%, 'Color',cg(i,:))
@@ -241,6 +288,8 @@ ll = length(ax.Children); cg = clrmp(ll);%cgrad3([140 81 10]./255, [0.8 0.8 0.8]
 ax.ColorOrder = cg;
 
 
+nexttile
+funprep
 
 nexttile
 for i=1:5:size(Edef_p,2)
@@ -262,10 +311,25 @@ ax=gca;
 ll = length(ax.Children); cg = clrmp(ll);%cgrad3([140 81 10]./255, [0.8 0.8 0.8], [1 102 94]./255, ll);
 ax.ColorOrder = cg;
 
-sgtitle(sprintf('Simulation of E from dipole with \\Theta = %d° and \\Phi = %d°: %s',round(rad2deg(theta_dip)), round(rad2deg(phi_dip)),mode))
+sgtitle(sprintf('Simulation of E from dipole with \\Theta = %.0f° and \\Phi = %.0f°: %s',round(rad2deg(theta_dip)), round(rad2deg(phi_dip)),mode))
 end
 
 function DOP = getDOP(Evsphi)
 	res = (max(Evsphi)-min(Evsphi))./(max(Evsphi)+min(Evsphi));
 DOP = res;
+end
+
+
+function [x, y, z] = spherical2cart(r, theta,phi)
+x = zeros(size(r,1),size(r,2));
+y = zeros(size(r,1),size(r,2));
+z = zeros(size(r,1),size(r,2));
+
+for i=1:length(x)
+	for j=1:length(y)
+	x(i,j) = r(j,i)*sin(theta(i))*sin(phi(j));
+	y(i,j) = r(j,i)*sin(theta(i))*cos(phi(j));
+	z(i,j) = r(j,i)*cos(theta(i));
+	end
+end
 end
